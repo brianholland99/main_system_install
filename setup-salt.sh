3718af3385f6b025235313f1f84f2b9578783c77
Brian Holland <brianholland99@gmail.com>
2020-04-01 13:40:24 -0400

    Checkpoint of work before switching to Ubuntu 20.04

--- setup-salt.sh
+++ setup-salt.sh
@@ -6,17 +6,17 @@ fi
 MINION_ID=$1
 SALT_PKI=/etc/salt/pki
 MIN_KEY_DIR=${SALT_PKI}/minion
+MASTER_MINS_KEY_DIR=${SALT_PKI}/master/minions
 
-# Get bootstrap_salt.sh
-curl -o bootstrap_salt.sh -L https://bootstrap.saltstack.com
+wget -O bootstrap-salt.sh https://bootstrap.saltstack.com
+sh bootstrap-salt.sh -M -N
 
-sh bootstrap_salt.sh -M -N  # Install just master.  This includes salt-key.
-mkdir ${MIN_KEY_DIR}  # Master install did not create this dir.
+mkdir -p ${MIN_KEY_DIR}  # Master install did not create this dir.
 salt-key --gen-keys-dir=${MIN_KEY_DIR} --gen-keys=minion
 
 # Now pre-seed master
 cp ${MIN_KEY_DIR}/minion.pub ${SALT_PKI}/master/minions/${MINION_ID}
 
 # Install minion
-sh bootstrap_salt.sh -i ${MINION_ID} -A 127.0.0.1
-rm bootstrap_salt.sh
+sh bootstrap-salt.sh -i bhdesk -A 127.0.0.1
+rm bootstrap-salt.sh
